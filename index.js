d0c86f086f96cddde6c15aa9851f273e27fdd019
Achyut-Behera <39526301+Achyut-Behera@users.noreply.github.com>
2018-09-14 12:04:06 +0530

    Index page changed

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ var app = express();
 const wx = new Wechat({
   "wechatToken": "weixin", //same as "Token(令牌)" in the form above
   "appId": "wxd9de598a2e8ea592",
-  "appSecret": "bad6a5b59572aea6c4ac92e14682a367",
+  "appSecret": "bad6a5b59572aea6c4ac92e14682a367"
 });
 
 app.get('/api/wechat', function(req, res){
+    console.log("request query :"+req.query);
     if(wx.jssdk.verifySignature(req.query)) {
       res.send(req.query.echostr);
       return;
